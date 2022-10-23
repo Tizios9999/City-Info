e9f84df56b0d34f8234bbb29f303d74ced88838a
Tizios9999 <davide.santonocito@outlook.com>
2022-10-23 15:21:50 +0200

    first setup

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,12 @@
 import '../sass/style.scss';
 import testConsole from './secondary';
 
-function testDraw() {
+function draw() {
     const el = document.createElement('div');
     el.innerHTML = "Test";
     el.classList.add('test-class');
     return el;
 }
 
-document.body.appendChild(testDraw());
+document.body.appendChild(draw());
 testConsole();
