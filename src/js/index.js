2993c6dba06b1aab302f738480992fcffc3bdeb7
Tizios9999 <davide.santonocito@outlook.com>
2022-10-26 09:47:26 +0200

    category box template setup

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,33 @@
 import '../sass/style.scss';
 import testConsole from './secondary';
 
+let urbanList = [
+        {
+            "href": "link1", 
+            "name": "San Diego"
+        }, 
+        {
+            "href": "link2", 
+            "name": "Domodossola"
+        }, 
+        {
+            "href": "link3", 
+            "name": "Milano"
+        }, 
+        {
+            "href": "link4", 
+            "name": "Mestre"
+        }, 
+        {
+            "href": "link5", 
+            "name": "San Francisco"
+        }, 
+        {
+            "href": "link6", 
+            "name": "Santiago"
+        }, 
+];
+
 function draw() {
     const el = document.createElement('div');
     el.innerHTML = "Test";
